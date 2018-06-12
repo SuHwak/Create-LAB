37bed28b4b96b1c77a818f84a5880bb7de97da70
Micha Vermeer <github@mivex.net>
2018-06-12 15:55:01 +0200

    VS Code does not support message boxes...

--- Create-LAB.ps1
+++ Create-LAB.ps1
@@ -1,12 +1,13 @@
-# Assuming Set-ExecutionPolicy RemoteSigned Test
+# Assuming Set-ExecutionPolicy RemoteSigned
 
 # Loading Modules
-. "C:\Users\mverm\OneDrive\Tools\PowerShell Scripts and Commandlets\Get-FileName.ps1"
-. "C:\Users\mverm\OneDrive\Tools\PowerShell Scripts and Commandlets\Convert-WindowsImage.ps1"
+# . "C:\Users\mverm\OneDrive\Tools\PowerShell Scripts and Commandlets\Get-FileName.ps1"
+# . "C:\Users\mverm\OneDrive\Tools\PowerShell Scripts and Commandlets\Convert-WindowsImage.ps1"
 
 # Setting Variables
 
 $domainController = "MivexLab-DC1"
+$domainName = "MIVEX.LAB"
 $memberServersCount = 2
 $ISOlocation = $null
 $ethernet = Get-NetAdapter -Name Ethernet
@@ -15,8 +16,8 @@ $wifi = Get-NetAdapter -Name Wi-Fi
 # Set Administrator Password
 
 $adminUsername = "Administrator"
-$adminPassword = "Passw0rd"
-$adminPasswordNano = ConvertTo-SecureString -String $adminPassword -AsPlainText -Force 
+$adminPassword = "Passw0rd" 
+$adminSecPassword = $adminPassword | ConvertTo-SecureString -AsPlainText -Force
 
 $secstr = New-Object -TypeName System.Security.SecureString
 $adminPassword.ToCharArray() | ForEach-Object {$secstr.AppendChar($_)}
@@ -25,33 +26,7 @@ $credentials = new-object -typename System.Management.Automation.PSCredential -a
 if (!$ISOlocation)
     {
 
-    $ISOlocation = Get-FileName -initialDirectory "G:\ISO's" -FileType "ISO"
-
-    While ($ISOlocation -notlike ".iso")
-        {
-
-        $msgBoxInput =  [System.Windows.MessageBox]::Show('Selected file is not an ISO','Invalid File','OkCancel','Error')
-        switch  ($msgBoxInput) 
-            {
-
-            'OK' 
-                {
-
-                ## Do something 
-
-                }
-
-            'Cancel' 
-                {
-
-                # Without the ISO we can't continue
-                break
-
-                }
-
-            }
-
-        }
+    $ISOLocation = Read-Host -prompt "Please provide the path to the ISO file."
 
     }
 
