8a9314a44b5d028222fdef4dab787501b710262d
Micha Vermeer <github@mivex.net>
2018-06-14 18:22:39 +0200

    Copy Wim File

--- Create-LAB.ps1
+++ Create-LAB.ps1
@@ -9,9 +9,11 @@
 $domainController = "MivexLab-DC1"
 $domainName = "MIVEX.LAB"
 $memberServersCount = 2
-$ISOlocation = $null
-$ethernet = Get-NetAdapter -Name Ethernet
-$wifi = Get-NetAdapter -Name Wi-Fi
+$ISOlocation = $null # will contain the path to the ISO file we use to build an image
+$InstallWimLocation = $null # Will contain the path where the WIM file will be copied to and saved for future use
+
+# $ethernet = Get-NetAdapter -Name Ethernet
+# $wifi = Get-NetAdapter -Name Wi-Fi
 
 # Set Administrator Password
 
@@ -33,24 +35,38 @@ if (!$ISOlocation) {
             $ISOlocation = $null
         
         }
+
+        $mountResult = Mount-DiskImage -ImagePath $ISOlocation -PassThru
+        
+        $driveLetter = ($mountResult | Get-Volume).DriveLetter + ":"
+
+        $OriginalWimFile = Get-ChildItem -path $driveLetter -Filter install.wim -Recurse -ErrorAction SilentlyContinue
+        if  (!($OriginalWimFile)) {
+            Write-Host -ForegroundColor red "$isolocation mounted on $driveletter does not contain a install.wim file"
+            Dismount-DiskImage $ISOlocation
+            $ISOlocation = $null
+        }
     }
 }
 
 
-<#
-While ($ISOlocation -notlike ".iso") {
-    
-    $ISOlocation = Get-FileName -initialDirectory "G:\ISO's" -FileType "ISO"
+if (!$InstallWimLocation) {
+    while (!($InstallWimLocation)) {
+        
+        $InstallWimLocation = Read-Host -prompt "Please provide the path to where the install.wim file may be copied to."
 
+        if (!(Test-Path $InstallWimLocation -ErrorAction SilentlyContinue) -or (Get-ChildItem $InstallWimLocation -Recurse).count -ne 0) { # Test-Path tests if the folder exists, 
+                                                                                                                        # counterintuitively, get-childitem returns True if it is empty, and we want the folder to be empty
+        
+            Write-Host -ForegroundColor Red "$InstallWimLocation does not exist or is not empty."
+            $InstallWimLocation = $null
+        
+        }
 
-
-
+        else {
+            Write-Host -ForegroundColor Green "Copying, please wait"
+            Copy-Item -Path $OriginalWimFile.FullName -Destination $InstallWimLocation
+        }
+    }
 }
-#>
 
-$mountResult = Mount-DiskImage -ImagePath $ISOlocation -PassThru
-
-$driveLetter = ($mountResult | Get-Volume).DriveLetter + ":"
-
-If (! (Test-Path $destination)) { mkdir $destination }
-Set-Location $destination
