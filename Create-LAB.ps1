730a0d68521c0d65fda5b7bdc14dc0cafbede981
Micha Vermeer <github@mivex.net>
2018-06-15 15:29:00 +0200

    Removed redundant if statements

--- Create-LAB.ps1
+++ Create-LAB.ps1
@@ -27,59 +27,58 @@ $adminPassword.ToCharArray() | ForEach-Object {$secstr.AppendChar($_)}
 $credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $adminUsername, $secstr
 
 # Find out the ISO file location which contains the Windows image
-if (!$ISOlocation) {
-    while ($ISOlocation -notlike "*.iso") {
-        
-        $ISOlocation = Get-FileName -initialDirectory "G:\ISO's" -FileType "ISO"
-        
-        # $ISOLocation = Read-Host -prompt "Please provide the path to the ISO file."
-        if (!(Get-ChildItem $ISOlocation -ErrorAction SilentlyContinue) -or $ISOlocation -notlike "*.iso") {
-        
-            Write-Host -ForegroundColor Red "$ISOlocation does not exist or is not an ISO file."
-            $ISOlocation = $null
-        
-        }
+while ($ISOlocation -notlike "*.iso") {
+    
+    $ISOlocation = Get-FileName -initialDirectory "G:\ISO's" -FileType "ISO"
+    
+    # $ISOLocation = Read-Host -prompt "Please provide the path to the ISO file."
+    if (!(Get-ChildItem $ISOlocation -ErrorAction SilentlyContinue) -or $ISOlocation -notlike "*.iso") {
+    
+        Write-Host -ForegroundColor Red "$ISOlocation does not exist or is not an ISO file."
+        $ISOlocation = $null
+    
+    }
 
-        $mountResult = Mount-DiskImage -ImagePath $ISOlocation -PassThru
-        
-        $driveLetter = ($mountResult | Get-Volume).DriveLetter + ":"
+    $mountResult = Mount-DiskImage -ImagePath $ISOlocation -PassThru
+    
+    $driveLetter = ($mountResult | Get-Volume).DriveLetter + ":"
 
-        $OriginalWimFile = Get-ChildItem -path $driveLetter -Filter install.wim -Recurse -ErrorAction SilentlyContinue
-        if  (!($OriginalWimFile)) {
-            Write-Host -ForegroundColor red "$isolocation mounted on $driveletter does not contain a install.wim file"
-            Dismount-DiskImage $ISOlocation
-            $ISOlocation = $null
-        }
+    $OriginalWimFile = Get-ChildItem -path $driveLetter -Filter install.wim -Recurse -ErrorAction SilentlyContinue
+    if  (!($OriginalWimFile)) {
+        Write-Host -ForegroundColor red "$isolocation mounted on $driveletter does not contain a install.wim file"
+        Dismount-DiskImage $ISOlocation
+        $ISOlocation = $null
     }
 }
+
 
 # Find the location where the WIM file might be copied to
-if (!$InstallWimLocation) {
-    while (!($InstallWimLocation)) {
+while (!($InstallWimLocation)) {
 
-        $InstallWimLocation = Get-FolderName 
-        
-        # $InstallWimLocation = Read-Host -prompt "Please provide the path to where the install.wim file may be copied to."
+    $InstallWimLocation = Get-FolderName 
+    
+    # $InstallWimLocation = Read-Host -prompt "Please provide the path to where the install.wim file may be copied to."
 
-        if (!(Test-Path $InstallWimLocation -ErrorAction SilentlyContinue) -or (Get-ChildItem $InstallWimLocation -Recurse).count -ne 0) { # Test-Path tests if the folder exists, 
-                                                                                                                        # counterintuitively, get-childitem returns True if it is empty, and we want the folder to be empty
-        
-            Write-Host -ForegroundColor Red "$InstallWimLocation does not exist or is not empty."
-            $InstallWimLocation = $null
-        
-        }
+    if (!(Test-Path $InstallWimLocation -ErrorAction SilentlyContinue) -or (Get-ChildItem $InstallWimLocation -Recurse).count -ne 0) { # Test-Path tests if the folder exists, 
+                                                                                                                    # counterintuitively, get-childitem returns True if it is empty, and we want the folder to be empty
+    
+        Write-Host -ForegroundColor Red "$InstallWimLocation does not exist or is not empty."
+        $InstallWimLocation = $null
+    
+    }
 
-        else {
-            Write-Host -ForegroundColor Green "Copying, please wait"
+    else {
+        Write-Host -ForegroundColor Green "Copying, please wait"
 
-            Start-BitsTransfer -Source $OriginalWimFile.FullName -Destination $InstallWimLocation -Description "Copying Install.wim file" -DisplayName "Copying..." 
-            # Copy-Item -Path $OriginalWimFile.FullName -Destination $InstallWimLocation
-        }
+        Start-BitsTransfer -Source $OriginalWimFile.FullName -Destination $InstallWimLocation -Description "Copying Install.wim file" -DisplayName "Copying..." 
+        # Copy-Item -Path $OriginalWimFile.FullName -Destination $InstallWimLocation
     }
 }
 
 
 
+
+# Create the virtual switches if they are missing 
 $LabSwitches = Get-VMSwitch | Where-Object{$_.Name -match "LAB"}
 
 if ($LabSwitches.Name -notmatch "LAB-OUTSIDE") {
@@ -107,3 +106,4 @@ if ($LabSwitches.Name -notmatch "LAB-INSIDE") {
     Write-Host -ForegroundColor Yellow "Switch LAB-INSIDE does not yet exist, creating it now..."
     New-VMSwitch -SwitchType Private -Name "LAB-INSIDE"
 }
+
