794c8f14530438f0600cfa10b72613fb113f2f7c
Micha Vermeer <github@mivex.net>
2018-06-19 15:43:39 +0200

    We have bootable updated VHDx files!

--- Create-LAB.ps1
+++ Create-LAB.ps1
@@ -14,7 +14,7 @@ $InstallWimLocation = $null # Will contain the path where the WIM file will be c
 $WimFile = $null
 $WinDCCoreFileName = "WinDCCore.wim"
 $WinDCGuiFileName = "WinDCGui.wim"
-$WindowsUpdatesLocation = "G:\ISO's\Microsoft\WinUpdates\w100-x64\glb"
+$WindowsUpdatesLocation = "G:\ISO's\Microsoft\Windows Server\WindowsServer2016-Updates"
 $LabSwitches = @()
 
 # Set Administrator Password
@@ -51,7 +51,7 @@ while (!($WimFile)) {
         while ($ISOlocation -notlike "*.iso") {
     
             $ISOlocation = Get-FileName -initialDirectory "G:\ISO's" -FileType "ISO"
-            Write-Host -ForegroundColor Green "Checking if this is a good iso file"
+            Write-Host -ForegroundColor Green "Checking if $isolocation is a good iso file"
             
             if (!(Get-ChildItem $ISOlocation -ErrorAction SilentlyContinue) -or $ISOlocation -notlike "*.iso") {
             
@@ -82,18 +82,21 @@ while (!($WimFile)) {
         
         Write-Host -ForegroundColor Green "Done"
 
-        
+        Write-Host -ForegroundColor Yellow "We now have the install.wim file, but we need Convert-WindowsImage script from the ISO as well"
+
+        Get-ChildItem -path $driveLetter -Include Convert-WindowsImage.ps1 -Recurse | Copy-Item -Destination $InstallWimLocation
+   
     }
 
     if ($mountResult) {
         Write-Host -ForegroundColor Green "Dismounting the iso."
+        
         Dismount-DiskImage $mountResult.ImagePath
     }
-
 }
 
 Write-Host -ForegroundColor Yellow "Checking if the Windows Datacenter Core edition wim file already exists"
-$WinDCCoreWimFile = Get-ChildItem -Path $InstallWimLocation -Filter $WinDCCoreFileName -ErrorAction SilentlyContinue | select -Property Name,FullName,Directory
+$WinDCCoreWimFile = Get-ChildItem -Path $InstallWimLocation -Filter $WinDCCoreFileName -ErrorAction SilentlyContinue | select -Property Name,BaseName,FullName,Directory,LastWriteTime
 
 if (!$WinDCCoreWimFile) {
 
@@ -109,7 +112,7 @@ if (!$WinDCCoreWimFile) {
 }
 Write-Host -ForegroundColor Green "Found or created the Windows Datacenter Core edition wim file"
 Write-Host -ForegroundColor Yellow "Checking if the Windows Datacenter Gui edition wim file already exists"
-$WinDCGuiWimFile = Get-ChildItem -Path $InstallWimLocation -Filter $WinDCGuiFileName -ErrorAction SilentlyContinue | select -Property Name,FullName,Directory
+$WinDCGuiWimFile = Get-ChildItem -Path $InstallWimLocation -Filter $WinDCGuiFileName -ErrorAction SilentlyContinue | select -Property Name,BaseName,FullName,Directory,LastWriteTime
 
 if (!$WinDCGuiWimFile) {
 
@@ -126,37 +129,95 @@ if (!$WinDCGuiWimFile) {
 Write-Host -ForegroundColor Green "Found or created the Windows Datacenter Gui edition wim file"
 
 Write-Host -ForegroundColor Yellow "Searching for any updates in $WindowsUpdatesLocation"
+
+# avoid downloading updates that we already have
+$PreviouslyDoneUpdates =  Get-ChildItem -path $WindowsUpdatesLocation\done\* -Include *.cab, *.msu
+
+# Retrieving new updates
+
+Write-Host -fore green "Checking Windows Update for updates"
+& 'G:\ISO''s\Microsoft\wsusoffline\cmd\DownloadUpdates.cmd' w100-x64 glb /verify /includewddefs /includedotnet
+
+Write-Host -fore green "Copying new updates, please wait..."
+Get-ChildItem -path "G:\ISO's\Microsoft\wsusoffline\client\w100-x64\glb\*" -Include *.cab, *.msu | where{$PreviouslyDoneUpdates.name -notcontains $_.Name} | %{Write-Host "Copying $_"; Copy-Item $_ -Destination $WindowsUpdatesLocation}
+
 $UpdateFiles = Get-ChildItem -path $WindowsUpdatesLocation\* -Include *.cab, *.msu | sort LastWriteTime
 
-Write-Host -ForegroundColor Green "Found $($Updatefiles.count) update(s)"
+Write-Host -ForegroundColor Green "Found $($Updatefiles.count) new update(s)"
 
-if (!(Test-Path "G:\MountedWimDCCore")) {
-    New-Item -ItemType "Directory" -Path G:\MountedWimDCCore
+if ($Updatefiles) {
+        
+    if (!(Test-Path "G:\MountedWimDCCore")) {
+        New-Item -ItemType "Directory" -Path G:\MountedWimDCCore
+    }
+    if (!(Test-Path "G:\MountedWimDCGui")) {
+        New-Item -ItemType "Directory" -Path G:\MountedWimDCGui
+    }
+    if (!(Test-Path "$WindowsUpdatesLocation\Done")) {
+        New-Item -ItemType "Directory" -Path "$WindowsUpdatesLocation\Done"
+    }
+
+    Write-Host -ForegroundColor Green "Mounting the Windows Server wim files to be able to apply updates"
+    $DISMMountResultWinDCCore = Mount-WindowsImage -ImagePath $WinDCCoreWimFile.FullName -path "G:\MountedWimDCCore" -Index 1 
+    $DISMMountResultWinDCGui = Mount-WindowsImage -ImagePath $WinDCGuiWimFile.FullName -path "G:\MountedWimDCGui" -Index 1 
+
+    Write-Host -ForegroundColor Green "Updating..."
+    foreach ($UpdateFile in $UpdateFiles) {
+        Write-Host -ForegroundColor Green "Applying Update $($UpdateFile.Name)"
+        Add-WindowsPackage -Path $DISMMountResultWinDCCore.Path -PackagePath $UpdateFile.FullName -LogPath G:\DISMLog\DismDCCore.log -ErrorAction SilentlyContinue
+        Add-WindowsPackage -Path $DISMMountResultWinDCGui.Path -PackagePath $UpdateFile.FullName -LogPath G:\DISMLog\DismDCGui.log -ErrorAction SilentlyContinue
+
+        Move-Item $UpdateFile.FullName -Destination "$WindowsUpdatesLocation\Done" -Force
+    }
+
+    Write-Host -ForegroundColor Green "Done applying updates, dismounting the wim files, saving changes"
+
+    Dismount-WindowsImage -path $DISMMountResultWinDCCore.path -Save
+    Dismount-WindowsImage -path $DISMMountResultWinDCGui.path -Save
+
+    Write-Host -ForegroundColor Green "Done saving the images into the wim files"
 }
-if (!(Test-Path "G:\MountedWimDCGui")) {
-    New-Item -ItemType "Directory" -Path G:\MountedWimDCGui
+
+# .-loading the convert-windowsimage script now.
+. $InstallWimLocation\Convert-WindowsImage.ps1
+
+
+Write-Host -ForegroundColor Green "Checking if we need to convert the wim files to vhdx files so we can boot from them"
+
+$WinDCCoreVHDXFile = "$($WinDCCoreWimFile.Directory)\$($WinDCCoreWimFile.BaseName).vhdx"
+$WinDCGuiVHDXFile = "$($WinDCGuiWimFile.Directory)\$($WinDCGuiWimFile.BaseName).vhdx"
+$WinDCCoreVHDXFileExists = Test-Path $WinDCCoreVHDXFile
+$WinDCGuiVHDXFileExists = Test-Path $WinDCGuiVHDXFile
+
+if (!$WinDCCoreVHDXFileExists) {
+    Write-Host -fore Yellow "We DO NOT have a VHDX, converting the WIM file to VHDX"
+
+    Write-Host -NoNewline -Fore Yellow "Converting "; Write-Host -NoNewline -Fore Blue $WinDCCoreWimFile.FullName; Write-Host -NoNewline -Fore Yellow " to "; Write-Host -Fore Blue $WinDCCoreVHDXFile
+    Convert-WindowsImage -SourcePath $WinDCCoreWimFile.FullName -VHDPath $WinDCCoreVHDXFile -DiskLayout UEFI
+    
 }
-if (!(Test-Path "$WindowsUpdatesLocation\Done")) {
-    New-Item -ItemType "Directory" -Path "$WindowsUpdatesLocation\Done"
+elseif ((Get-ChildItem $WinDCCoreVHDXFile).LastWriteTime -lt $WinDCCoreWimFile.LastWriteTime) {
+    Write-Host -fore Yellow "We have an outdated VHDX, converting the newer WIM file to VHDX"
+    
+    Write-Host -NoNewline -Fore Yellow "Converting "; Write-Host -NoNewline -Fore Blue $WinDCCoreWimFile.FullName; Write-Host -NoNewline -Fore Yellow " to "; Write-Host -Fore Blue $WinDCCoreVHDXFile
+    Convert-WindowsImage -SourcePath $WinDCCoreWimFile.FullName -VHDPath $WinDCCoreVHDXFile -DiskLayout UEFI
+
 }
 
-Write-Host -ForegroundColor Green "Mounting the Windows Server wim files to be able to apply updates"
-$DISMMountResultWinDCCore = Mount-WindowsImage -ImagePath $WinDCCoreWimFile.FullName -path "G:\MountedWimDCCore" -Index 1 
-$DISMMountResultWinDCGui = Mount-WindowsImage -ImagePath $WinDCGuiWimFile.FullName -path "G:\MountedWimDCGui" -Index 1 
+if (!$WinDCGuiVHDXFileExists) {
+    Write-Host -fore Yellow "We DO NOT have a VHDX, converting the WIM file to VHDX"
 
-Write-Host -ForegroundColor Green "Updating..."
-foreach ($UpdateFile in $UpdateFiles) {
-    Write-Host -ForegroundColor Green "Applying Update $($UpdateFile.Name)"
-    Add-WindowsPackage -Path $DISMMountResultWinDCCore.Path -PackagePath $UpdateFile.FullName -LogPath G:\DISMLog\DismDCCore.log -ErrorAction SilentlyContinue
-    Add-WindowsPackage -Path $DISMMountResultWinDCGui.Path -PackagePath $UpdateFile.FullName -LogPath G:\DISMLog\DismDCGui.log -ErrorAction SilentlyContinue
+    Write-Host -NoNewline -Fore Yellow "Converting "; Write-Host -NoNewline -Fore Blue $WinDCGuiWimFile.FullName; Write-Host -NoNewline -Fore Yellow " to "; Write-Host -Fore Blue $WinDCGuiVHDXFile
+    Convert-WindowsImage -SourcePath $WinDCGuiWimFile.FullName -VHDPath $WinDCGuiVHDXFile -DiskLayout UEFI
 
-    Move-Item $UpdateFile.FullName -Destination "$WindowsUpdatesLocation\Done" -Force
 }
 
-Write-Host -ForegroundColor Green "Done applying updates, dismounting the wim files, saving changes"
+elseif ((Get-ChildItem $WinDCGuiVHDXFile).LastWriteTime -lt $WinDCGuiWimFile.LastWriteTime) {
+    Write-Host -fore Yellow "We have an outdated VHDX, converting the newer WIM file to VHDX"
 
-Dismount-WindowsImage -path $DISMMountResultWinDCCore.path -Save
-Dismount-WindowsImage -path $DISMMountResultWinDCGui.path -Save
+    Write-Host -NoNewline -Fore Yellow "Converting "; Write-Host -NoNewline -Fore Blue $WinDCGuiWimFile.FullName; Write-Host -NoNewline -Fore Yellow " to "; Write-Host -Fore Blue $WinDCGuiVHDXFile
+    Convert-WindowsImage -SourcePath $WinDCGuiWimFile.FullName -VHDPath $WinDCGuiVHDXFile -DiskLayout UEFI
+}
 
 Write-Host -ForegroundColor Green "Creating the vSwitches if they do not exist yet"
 
